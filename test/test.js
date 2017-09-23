4b15b9faadd09ce03e817f53977d178416dc49e4
Richard Marquez <richard92m@me.com>
2017-09-22 21:07:18 -0700

    passing test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('server', function() {
+  describe('testFunc()', function() {
+    it('should save without error', function() {
+      expect(3).to.equal(3);
+    });
+  });
+});
