5c7f54d505dff2767811d227e723a75e58ea805d
ramarquez1992 <ramarquez1992@gmail.com>
2020-04-08 12:06:01 -0500

    workflow test

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <Counter />
+        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
