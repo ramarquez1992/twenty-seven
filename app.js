b70f721cd46edd1d7895122bd18ca0f6b20863e6
ramarquez1992 <ramarquez1992@gmail.com>
2020-04-07 20:46:05 -0500

    updated index path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/users', usersRouter);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 module.exports = app;
