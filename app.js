0e1d25011748913d5c1c67f3d3ece9acc2fa6f99
ramarquez1992 <ramarquez1992@gmail.com>
2020-04-07 20:53:58 -0500

    proper path

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/users', usersRouter);
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 module.exports = app;
