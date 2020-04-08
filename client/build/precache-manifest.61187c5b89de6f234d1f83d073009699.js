3828c535aa21704c08c679f869538c3b5bb36501
ramarquez1992 <ramarquez1992@gmail.com>
2020-04-07 20:56:41 -0500

    added client/build/

---
+++ client/build/precache-manifest.61187c5b89de6f234d1f83d073009699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565a2a68e48e06aa9b1403ecf8d44037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a29f9fbcd3cfbfcc901",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52e38ecc3802c09b1c12",
+    "url": "/static/js/2.87230573.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87230573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a29f9fbcd3cfbfcc901",
+    "url": "/static/js/main.b4a553c2.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
