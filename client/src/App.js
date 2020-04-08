8ec88adce8dd670631458e5586af2cc04ede7606
ramarquez1992 <ramarquez1992@gmail.com>
2020-04-07 21:10:04 -0500

    build on ebs

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to rload.
+              Edit <code>src/App.js</code> and save to reload pre-built.
             </p>
             <a
                 className="App-link"
